afb4b455c3805b8a2a10ab3b162a6557247819ab
Pepe Ferrer-Bonsoms Castan <pepeferrerbc@gmail.com>
2025-10-30 13:53:59 +0100

    Add initial implementation of basic arithmetic functions and dataset filtering

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+    "Age": [24, 30, 22, 35],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"],
+}
+df = pd.DataFrame(data)
+df = df[df["Age"] > 25]
